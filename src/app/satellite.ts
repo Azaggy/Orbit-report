f7cbb2547c0a2f725258dee7e1688d3753606fb1
unknown <anamariazaggy@gmail.com>
2020-12-25 16:31:56 -0600

    figuring out shouldShowWarning

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,9 +1,10 @@
 export class Satellite {
     name: string;
-    orbitType: string;
     type: string;
     operational: boolean;
+    orbitType: string;
     launchDate: string;
+    shouldShowWarning: boolean = true;
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
